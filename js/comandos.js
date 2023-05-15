aec12bb4f1cb715dda2784435eb9d5d19a56bdf4
maykprogramador <133047863+maykprogramador@users.noreply.github.com>
2023-05-14 22:04:47 -0500

    Update comandos.js

--- js/comandos.js
+++ js/comandos.js
@@ -65,23 +65,36 @@ function gameOverr(){
 function mensajes(){
     document.getElementById('vidajug').innerHTML=vidaJugador
     document.getElementById('vidaenem').innerHTML=vidaEnemigo
-    let sectionMensajes =document.getElementById('mensajes')
-    let parrafo = document.createElement('p')
-    parrafo.innerHTML = ataquejugador+" , "+enemigo+"--"+gameover+"--"
+    
+    let sectionMensajes =document.getElementById('resultado')
+    let ataquesjugador = document.getElementById('ataques-jugador')
+    let ataquesenemigo = document.getElementById('ataques-enemigo')
+    
+    /*parrafo.innerHTML = ataquejugador+" , "+enemigo+"--"+gameover+"--"*/
+    let nuevoataquesjugador= document.createElement('p')
+    let nuevoataquesenemigo= document.createElement('p')
+    
+    sectionMensajes.innerHTML=gameover
+    nuevoataquesjugador.innerHTML=ataquejugador
+    nuevoataquesenemigo.innerHTML=enemigo
+    
+    ataquesjugador.appendChild(nuevoataquesjugador)
+    ataquesenemigo.appendChild(nuevoataquesenemigo)
 
-    sectionMensajes.appendChild(parrafo)
-}
+}   
+
+
 
 function ataqueEnemigo(){
     let random=Math.ceil(Math.random()*3)
     if(random == 1){
-        enemigo ="MASCOTA ENEMIGO ATACA CON FUEGO"
+        enemigo ="FUEGO"
         contenemigo = 1
     }else if(random == 2){
-        enemigo ="MASCOTA ENEMIGO ATACA CON AGUA"
+        enemigo ="AGUA"
         contenemigo =2
     }else{
-        enemigo ="MASCOTA ENEMIGA ATACA CON TIERRA"
+        enemigo ="TIERRA"
         contenemigo =3
     }
     
@@ -92,30 +105,30 @@ function ataqueEnemigo(){
     
 }
 function ataquefuego (){
-    ataquejugador ="TU MASCOTA ATACO CON FUEGO" 
+    ataquejugador ="FUEGO" 
     contjugador = 1
     //alert(ataquejugador)
     ataqueEnemigo()
 }
 function ataqueagua (){
-    ataquejugador ="TU MASCOTA ATACO CON AGUA"
+    ataquejugador ="AGUA"
     contjugador = 2
    // alert(ataquejugador)
     ataqueEnemigo()
 }
 function ataquetierra (){
-    ataquejugador ="TU MASCOTA ATACO CON TIERRA" 
+    ataquejugador ="TIERRA" 
     contjugador = 3
     //alert(ataquejugador)
     ataqueEnemigo()
 }
 function seleccionarMascotaEnemigo(){
     resultado = Math.ceil(Math.random() *3)
-    if(resultado == 1){document.getElementById('mascota-enemigo').innerHTML="Fuego fresquito"
+    if(resultado == 1){document.getElementById('mascota-enemigo').innerHTML="Dominuss"
     }
-    else if(resultado == 2){document.getElementById('mascota-enemigo').innerHTML="fenec"
+    else if(resultado == 2){document.getElementById('mascota-enemigo').innerHTML="fenecc"
     }
-    else{document.getElementById('mascota-enemigo').innerHTML="optane"}
+    else{document.getElementById('mascota-enemigo').innerHTML="optanee"}
 }
 
 function seleccionarMascotaJugador(){
@@ -123,24 +136,28 @@ function seleccionarMascotaJugador(){
     let ocultarReglas = document.getElementById('reglas')
     let ocultarmascota = document.getElementById('seleccionar-mascota')
     ocultarmascota.style.display='none'
-    ocultarReglas.style.display='block'
-    ocultarataques.style.display='block'
+    ocultarReglas.style.display='flex'
+    ocultarataques.style.display='flex'
     if(document.getElementById('mascota1').checked){
         //*alert("seleccionaste mascota1")
-        document.getElementById('mascota-jugador').innerHTML="Fuego fresquito"
+        document.getElementById('mascota-jugador').innerHTML="Dominuss"
         seleccionarMascotaEnemigo()
     }
     else if(document.getElementById('mascota2').checked){
         //alert("seleccionaste mascota2")
-        document.getElementById('mascota-jugador').innerHTML="fenec"
+        document.getElementById('mascota-jugador').innerHTML="fenecc"
         seleccionarMascotaEnemigo()
     }
     else if (document.getElementById('mascota3').checked){
        // alert("seleccionaste mascota3") 
-        document.getElementById('mascota-jugador').innerHTML="optane"
+        document.getElementById('mascota-jugador').innerHTML="optanee"
         seleccionarMascotaEnemigo()    
     }else{alert("selecciona una mascota")}
     
 }
 
 window.addEventListener('load',iniciarJuego)
+
+
+
+
